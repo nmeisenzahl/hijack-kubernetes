23773acda32a7aecb5d3e455c160fc37149b41fe
aSauerwein <andreas@sauerwein.se>
2023-02-21 22:47:49 +0100

    clone via https

--- assets/configure-vm.sh
+++ assets/configure-vm.sh
@@ -5,7 +5,6 @@ sudo apt-get update
 sudo apt-get install -y git python3 python3-pip
 
 # get log4j-shell-poc ready
-ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
-git clone git@github.com:kozmer/log4j-shell-poc.git
+git clone https://github.com/kozmer/log4j-shell-poc.git
 cd log4j-shell-poc
 pip3 install -r requirements.txt
